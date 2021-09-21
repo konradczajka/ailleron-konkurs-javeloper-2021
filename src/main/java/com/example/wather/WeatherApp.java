e81f2907ace6ee8b9e050139521623bed81a1f6e
Konrad Czajka <konrad.czajka@gmail.com>
2021-09-21 14:40:52 +0200

    Make the code work

--- src/main/java/com/example/wather/WeatherApp.java
+++ src/main/java/com/example/wather/WeatherApp.java
@@ -3,25 +3,39 @@ package com.example.wather;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+@FunctionalInterface
 interface WeatherConnector {
+
     String[] weather(String location);
 }
+
+@FunctionalInterface
 interface MailProvider {
+
     void sendMail(final String location, final String weatherDatum, final String datum);
 }
+
 public class WeatherApp {
 
     static final String[] locations = new String[]{"Cracow", "Warsaw", "London", "Lodz", "Kielce", "Tokyo", "NewYork", "Buenos Aires", "Rzeszow"};
+
+    private static final int executionLimitInSeconds = 10;
 
     public static void main(String[] args) throws InterruptedException {
 
         Random random = new Random();
 
-        Runnable task = () -> {
-            WeatherProviderUtilsCommonHelper provider = new WeatherProviderUtilsCommonHelper();
+        WeatherConnector dummyConnector = location -> new String[]{location, "20.0"};
+        MailProvider dummyMailProvider = (location, weatherDatum, datum) -> {};
 
-            String location = locations[random.nextInt()];
+        Runnable task = () -> {
+            WeatherProviderUtilsCommonHelper provider = new WeatherProviderUtilsCommonHelper(dummyConnector, dummyMailProvider);
+
+            String location = locations[random.nextInt(locations.length)];
 
             log(location);
 
@@ -30,8 +44,14 @@ public class WeatherApp {
             log(weather);
         };
 
-        for (int i = 0; i < locations.length * 20 ; i++) {
-            new Thread(task).join();
+        var pool = new ForkJoinPool(locations.length * 20);
+        try {
+            for (int i = 0; i < locations.length * 20; i++) {
+                pool.execute(task);
+            }
+            pool.awaitTermination(executionLimitInSeconds, SECONDS);
+        } finally {
+            pool.shutdown();
         }
     }
 
@@ -45,9 +65,15 @@ public class WeatherApp {
 }
 
 class WeatherProviderUtilsCommonHelper {
+
     private WeatherConnector weatherConnector;
     private MailProvider mailProvider;
     private Map<String, Weather> cacheWeather = new HashMap<>();
+
+    public WeatherProviderUtilsCommonHelper(WeatherConnector weatherConnector, MailProvider mailProvider) {
+        this.weatherConnector = weatherConnector;
+        this.mailProvider = mailProvider;
+    }
 
     public Weather checkWeatherAndSendMailWithTemperature(String location) {
 
@@ -81,11 +107,20 @@ class WeatherProviderUtilsCommonHelper {
 }
 
 class Weather {
+
     private String location;
     private double temp;
 
     public Weather(String location, double temp) {
         this.location = location;
         this.temp = temp;
+    }
+
+    @Override
+    public String toString() {
+        return "Weather{" +
+               "location='" + location + '\'' +
+               ", temp=" + temp +
+               '}';
     }
 }
